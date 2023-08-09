252884bf2b99d7f96e3e143bc5fd01a00481efb2
Chris Pastl <chris@crispybits.app>
2023-08-09 23:10:45 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # FROGehtaus-playlist-gen
+
+## Requirements
+- python3
+
+## Running
+Run script inside a directory containing two subdirectories each for input and output files. Defaults to `FROgehtaus Originalfiles` (source files) and `FROgehtaus for PL` (target directory).
+
+__Please note that existing files in the target directory will be overridden!__
+
+### Example
+```console
+cd path/to/FROgehtaus
+python3 generate_playlist.py
+```
+
+### Sample ouput
+
+```console
+Copied ./FROgehtaus Originalfiles/P_2023_09_10_beaverlab-teaser.mp3 to ./FROgehtaus for PL/FROGehtaus1.mp3
+Copied ./FROgehtaus Originalfiles/2023_08_12 Shaken'n Skate VAH.mp3 to ./FROgehtaus for PL/FROGehtaus2.mp3
+Copied ./FROgehtaus Originalfiles/2023_08_15_Greatest_hitz_audio.mp3 to ./FROgehtaus for PL/FROGehtaus3.mp3
+Copied ./FROgehtaus Originalfiles/2023_09_30_Qlash - HipHop.mp3 to ./FROgehtaus for PL/FROGehtaus4.mp3
+Copied ./FROgehtaus Originalfiles/2023_12_31_Clip Theater in der Innenstadt allgemein.mp3 to ./FROgehtaus for PL/FROGehtaus5.mp3
+Copied ./FROgehtaus Originalfiles/2023_12_31_FROgehtausTeaser.mp3 to ./FROgehtaus for PL/FROGehtaus6.mp3
+Copied ./FROgehtaus Originalfiles/2023_12_31_FROsch_Ankuender_Marina_58s.mp3 to ./FROgehtaus for PL/FROGehtaus7.mp3
+Copied ./FROgehtaus Originalfiles/2023_12_31_Jimgle25v3.mp3 to ./FROgehtaus for PL/FROGehtaus8.mp3
+``````
---
+++ generate_playlist.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from datetime import date, datetime
+import os
+import re
+import shutil
+
+
+SRC_DIR = "./FROgehtaus Originalfiles"  # path to source directory
+TGT_DIR = "./FROgehtaus for PL"         # path to target directory
+TGT_NAME = "FROGehtaus"                 # basename of copied file
+MAX_FILES = 10                          # max files to copy
+
+
+def generate_playlist(src_dir: str, start_date: date = date.today(), max_files: int = MAX_FILES) -> list[str]:
+    resre = []
+    respr = []
+    items = map(lambda f: os.path.join(src_dir, f), os.listdir(src_dir))
+    files = sorted(filter(lambda f: os.path.isfile(f), items))
+    for file in files:
+        match = re.search(r'\d{4}_\d{2}_\d{2}', os.path.basename(file))
+        if match is not None:
+            fdate = datetime.strptime(match.group(), '%Y_%m_%d').date()
+            if fdate >= start_date:
+                if re.search(r'^P_\d{4}_\d{2}_\d{2}', os.path.basename(file)):
+                    respr.append(file)
+                else:
+                    resre.append(file)
+    res = respr + resre
+    return res[0:max_files]
+
+
+def copy_files(src_names: [str], tgt_dir: str, tgt_name: str):
+    for idx, fname in enumerate(src_names):
+        sfx = os.path.splitext(fname)[-1]
+        tgt = os.sep.join([tgt_dir, "{}{}{}".format(tgt_name, idx+1, sfx)])
+        shutil.copyfile(fname, tgt)
+        print(f"Copied {fname} to {tgt}")
+
+
+playlist = generate_playlist(SRC_DIR)
+copy_files(playlist, TGT_DIR, TGT_NAME)
